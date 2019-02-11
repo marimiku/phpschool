1b5d6c7e701b1c7ffa90a9f0982bf5e39e5ef445
marimiku <mariamikulskaya@gmail.com>
2019-02-11 14:28:21 +0200

    init

---
+++ lesson5/bubble_sort.php
@@ -0,0 +1,19 @@
+<?php
+
+function bubble_sort($a)
+{
+    for ($i = 0; $i < count($a) - 1; $i++) {
+        for ($j = count($a) - 1; $j >= $i + 1; $j--) {
+            if ($a[$j] < $a[$j - 1]) {
+                $a[$j] = $a[$j] ^ $a[$j - 1];
+                $a[$j - 1] = $a[$j - 1] ^ $a[$j];
+                $a[$j] = $a[$j] ^ $a[$j - 1];
+            }
+            print_r($a);
+        }
+    }
+    return $a;
+}
+
+$a = [100, 7, 9, 468, -456, 56, 13, 6, -6];
+print_r(bubble_sort($a));
