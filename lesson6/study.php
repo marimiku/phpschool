af404af08f5cf6312942d2086ceb7bae338bcb6a
marimiku <mariamikulskaya@gmail.com>
2019-02-17 20:31:54 +0200

    Exercise 3

--- lesson6/study.php
+++ lesson6/study.php
@@ -7,8 +7,15 @@ $studentList = [new Student('Mike', 'Barnes', 0, 'freshman', 4),
     new Student('Jane', 'Miller', 1, 'senior', 3.6),
     new Student('Mary', 'Scott', 1, 'senior', 2.7)];
 
+$studyTimer = [60, 100, 40, 300, 1000];
+
 echo "Initial data:\n\n";
 foreach ($studentList as $student) {
     $student->showMyself();
 }
 
+echo "\nUpdated data:\n\n";
+foreach ($studentList as $key => $student) {
+    $student->studyTime($studyTimer[$key]);
+    $student->showMyself();
+}
