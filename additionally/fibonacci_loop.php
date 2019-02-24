4d0c2c150fb8aab69d04e1fd01fa0618c3739943
marimiku <mariamikulskaya@gmail.com>
2019-02-24 20:45:49 +0200

    init

---
+++ additionally/fibonacci_loop.php
@@ -0,0 +1,20 @@
+<?php
+
+function fibonacci($number)
+{
+    $fib1 = $fib2 = 1;
+    for ($i = 2; $i < $number; $i++) {
+        $temp = $fib1;
+        $fib1 = $fib2;
+        $fib2 += $temp;
+    }
+    return $fib2;
+}
+
+function fibonacci2($number)
+{
+    if ($number === 0) return 0;
+    return ($number > 0) ? fibonacci($number) : -fibonacci(-$number);
+}
+
+echo fibonacci2(9);
